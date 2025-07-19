05000e96ed142d5c70582900b06f66bd0b0c6ddd
binoooh <arvindacumos@gmail.com>
2025-07-19 01:24:06 -0700

    Updated MeetingView

---
+++ Scrumdinger/Scrumdinger/Views/MeetingHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by bino on 7/19/25.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingHeaderView()
+}
